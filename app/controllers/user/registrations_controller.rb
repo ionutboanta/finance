435254e2733317cdb097f15ce2ff11685649992f
Alex Harastasan <alexandru.haratasan@crisser.ro>
2017-02-21 11:19:04 +0000

    Update views and navigation

---
+++ app/controllers/user/registrations_controller.rb
@@ -0,0 +1,12 @@
+class User::RegistrationsController < Devise::RegistrationsController
+   before_filter :configure_permited_parameters
+   
+   
+   protected
+   
+   def configure_permited_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+   end
+    
+end
